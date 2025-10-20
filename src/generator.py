ce30ed674994ca2e9b6d4e9d18fae6903967bd5e
Muhammad Ali Khan <muhammadalikhanhitec@gmail.com>
2025-10-20 18:35:40 +0200

    Generator Model added for the project Mistrael

---
+++ src/generator.py
@@ -0,0 +1,20 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipelines
+
+def load_generator():
+    model_name = "mistralai/Mistral-7B-Instruct-v0.1"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        device_map = "auto",
+        load_in_4bit = True
+    )
+
+    pipe = pipelines("text-generation", model = model , tokenizer = tokenizer)
+
+    return pipe
+
+
+def generate_answer(pipe, context, question):
+    prompt = f"Use the context to answer the question. \n\nContext: \n{context}\n\nQuestion: {question}\nAnswer:"
+    out = pipe(prompt, max_new_tokens = 150, do_sample= False)
+    return out[0]["generated_text"]
