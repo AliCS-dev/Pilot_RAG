2e44af00bdfa9e0c4c172d113a87c49f8e713876
Muhammad Ali Khan <muhammadalikhanhitec@gmail.com>
2025-10-21 18:27:29 +0200

    Added time to check performance

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import time
 from loader import pdf_load_split
 from embedder import embed_texts
 from indexer import build_faiss_index,search
@@ -5,20 +6,25 @@ from generator import load_generator,generate_answer
 from verifier import load_verifier,verify_answer
 
 def main():
+    total_start = time.time()
     
     print("Loading PDF................")
     chunks = pdf_load_split("../data/sample.pdf")
     texts = [c.page_content for c in chunks]
 
+    start = time.time()  #checkpoint 
     print("Creating embeddings....... ")
     embeddings = embed_texts(texts)
 
+    start = time.time() #Chek point
     print("Building FAISS index ....")
     index = build_faiss_index(embeddings)
 
+    start = time.time() 
     print("Loading Microsoft PH.....")
     generator = load_generator()
 
+    start = time.time() #check point
     print("Loading LLamaa ............")
     verifier = load_verifier()
 
@@ -28,6 +34,8 @@ def main():
         q = input("Question: ")
         if q.lower() in ["exit","quit"]:
             break
+
+        start = time.time()
 
         q_embed = embed_texts([q])
         ids, _ = search(index, q_embed, k = 3)
@@ -39,6 +47,10 @@ def main():
 
         verified = verify_answer(verifier,context,q,answer)
         print("\n LLama double check the answers of Mic. PH: ",verified,"\n")
+
+        print(f"\nResponse generated in {round(time.time() - start, 2)} sec\n")
+
+    print(f"\nTotal runtime: {round(time.time() - total_start, 2)} sec")
 
 if __name__ == "__main__":
     main()
